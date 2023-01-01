8ba511a68d42278f67d5b83d8a5f10abd6da61ef
fgh <fenguhao@stu.pku.edu.cn>
2023-01-01 17:09:42 +0800

    tmp

--- src/VCX/Labs/4-Animation/tasks.cpp
+++ src/VCX/Labs/4-Animation/tasks.cpp
@@ -5,11 +5,79 @@
 #include <Eigen/Sparse>
 #include <iostream>
 #include <spdlog/spdlog.h>
-
+typedef Eigen::SparseMatrix<float> SPM;
+typedef Eigen::Triplet<float>      TRP;
+typedef Eigen::VectorXf            VECXF;
+typedef Eigen::SimplicialLLT<SPM>  LLT;
 namespace VCX::Labs::Animation {
-    typedef Eigen::SparseMatrix<float> SPM;
-    typedef Eigen::Triplet<float>      TRP;
-    void                               ForwardKinematics(IKSystem & ik, int StartIndex) {
+
+    void partial_g(MassSpringSystem & system, std::vector<glm::vec3> & x, std::vector<glm::vec3> & y, std::vector<glm::vec3> & b, float h) {
+        for (int i = 0; i < x.size(); i++) {
+            b[i] = system.Mass * (x[i] - y[i]) / (h * h);
+        }
+        for (auto const spring : system.Springs) {
+            auto const      p0  = spring.AdjIdx.first;
+            auto const      p1  = spring.AdjIdx.second;
+            glm::vec3 const x01 = x[p1] - x[p0];
+            glm::vec3 const v01 = ((x[p1] - system.Positions[p1]) - (x[p0] - system.Positions[p0])) / h;
+            glm::vec3 const e01 = glm::normalize(x01);
+            glm::vec3       f   = (system.Stiffness * (glm::length(x01) - spring.RestLength) + system.Damping * glm::dot(v01, e01)) * e01;
+            b[p0] -= f;
+            b[p1] += f;
+        }
+    }
+    void partial2_g(MassSpringSystem & system, std::vector<glm::vec3> & x, std::vector<glm::vec3> & y, std::vector<TRP> & A, float h) {
+        A.clear();
+        std::vector<glm::mat3x3> diag;
+        for (int i = 0; i < x.size(); i++) {
+            glm::mat3x3 Mi(0);
+            for (int j = 0; j < 3; j++) Mi[j][j] = system.Mass / (h * h);
+            diag.push_back(Mi);
+        }
+        for (auto const spring : system.Springs) {
+            auto const      p0   = spring.AdjIdx.first;
+            auto const      p1   = spring.AdjIdx.second;
+            glm::vec3 const x01  = x[p1] - x[p0];
+            glm::vec3 const x01_ = system.Positions[p1] - system.Positions[p0];
+            float           d01 = glm::length(x01), d01_ = glm::length(x01_);
+            glm::vec3 const v01 = ((x[p1] - system.Positions[p1]) - (x[p0] - system.Positions[p0])) / h;
+            glm::vec3 const e01 = glm::normalize(x01);
+            glm::mat3x3     Mij(0);
+            for (int i = 0; i < 3; i++) {
+                Mij[i][i] += -(system.Stiffness + system.Damping / h) + system.Stiffness * spring.RestLength * (glm::length2(x01) - x01[i] * x01[i]) / (glm::length2(x01) * glm::length(x01));
+                Mij[i][i] -= system.Damping / h * (((glm::dot(x01, x01_) + x01[i] * x01_[i]) * d01 * d01 - 2 * glm::dot(x01, x01_) * x01[i] * x01[i]) / (glm::length2(x01) * glm::length2(x01)));
+                diag[p0][i][i] += (system.Stiffness + system.Damping / h) - system.Stiffness * spring.RestLength * (glm::length2(x01) - x01[i] * x01[i]) / (glm::length2(x01) * glm::length(x01));
+                diag[p0][i][i] += system.Damping / h * (((glm::dot(x01, x01_) + x01[i] * x01_[i]) * d01 * d01 - 2 * glm::dot(x01, x01_) * x01[i] * x01[i]) / (glm::length2(x01) * glm::length2(x01)));
+                diag[p1][i][i] += (system.Stiffness + system.Damping / h) - system.Stiffness * spring.RestLength * (glm::length2(x01) - x01[i] * x01[i]) / (glm::length2(x01) * glm::length(x01));
+                diag[p1][i][i] += system.Damping / h * (((glm::dot(x01, x01_) + x01[i] * x01_[i]) * d01 * d01 - 2 * glm::dot(x01, x01_) * x01[i] * x01[i]) / (glm::length2(x01) * glm::length2(x01)));
+
+                for (int j = 0; j < 3; j++) {
+                    if (j == i) continue;
+                    Mij[i][j] += -2 * system.Stiffness * spring.RestLength * (x01[i] * x01[j]) / (glm::length2(x01) * glm::length(x01));
+                    diag[p0][i][j] += 2 * system.Stiffness * spring.RestLength * (x01[i] * x01[j]) / (glm::length2(x01) * glm::length(x01));
+                    diag[p1][i][j] += 2 * system.Stiffness * spring.RestLength * (x01[i] * x01[j]) / (glm::length2(x01) * glm::length(x01));
+                    Mij[i][j] += -system.Damping / h / (glm::length2(x01) * glm::length2(x01)) * (x01[i] * x01_[j] * glm::length2(x01) - x01[i] * glm::dot(x01, x01_) * 2 * x01[j]);
+                    diag[p0][i][j] += system.Damping / h / (glm::length2(x01) * glm::length2(x01)) * (x01[i] * x01_[j] * glm::length2(x01) - x01[i] * glm::dot(x01, x01_) * 2 * x01[j]);
+                    diag[p1][i][j] += system.Damping / h / (glm::length2(x01) * glm::length2(x01)) * (x01[i] * x01_[j] * glm::length2(x01) - x01[i] * glm::dot(x01, x01_) * 2 * x01[j]);
+                }
+            }
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    A.emplace_back(p0 * 3 + i, p1 * 3 + j, Mij[i][j]);
+                    A.emplace_back(p1 * 3 + j, p0 * 3 + i, Mij[i][j]);
+                }
+            }
+        }
+        for (int i = 0; i < x.size(); i++) {
+            for (int a = 0; a < 3; a++) {
+                for (int b = 0; b < 3; b++) {
+                    A.emplace_back(i * 3 + a, i * 3 + b, diag[i][a][b]);
+                }
+            }
+        }
+    }
+
+    void ForwardKinematics(IKSystem & ik, int StartIndex) {
         if (StartIndex == 0) {
             ik.JointGlobalRotation[0] = ik.JointLocalRotation[0];
             ik.JointGlobalPosition[0] = ik.JointLocalOffset[0];
@@ -94,37 +162,30 @@ namespace VCX::Labs::Animation {
 
     void AdvanceMassSpringSystem(MassSpringSystem & system, float const dt) {
         // your code here: rewrite following code
-        typedef Eigen::Triplet<float> Triplet;
-        int const                     steps = 1000;
-        float const                   ddt   = dt / steps;
+        int const   steps = 1000;
+        float const ddt   = dt / steps;
         for (std::size_t s = 0; s < steps; s++) {
-            /*
-            std::vector<glm::vec3> forces(system.Positions.size(), glm::vec3(0));
-            for (auto const spring : system.Springs) {
-                auto const      p0  = spring.AdjIdx.first;
-                auto const      p1  = spring.AdjIdx.second;
-                glm::vec3 const x01 = system.Positions[p1] - system.Positions[p0];
-                glm::vec3 const v01 = system.Velocities[p1] - system.Velocities[p0];
-                glm::vec3 const e01 = glm::normalize(x01);
-                glm::vec3       f   = (system.Stiffness * (glm::length(x01) - spring.RestLength) + system.Damping * glm::dot(v01, e01)) * e01;
-                forces[p0] += f;
-                forces[p1] -= f;
-            }
-            for (std::size_t i = 0; i < system.Positions.size(); i++) {
-                if (system.Fixed[i]) continue;
-                system.Velocities[i] += (glm::vec3(0, -system.Gravity, 0) + forces[i] / system.Mass) * ddt;
-                system.Positions[i] += system.Velocities[i] * ddt;
-            }
-            */
             auto                       y(system.Positions);
-            Eigen::SparseMatrix<float> A(3 * system.Positions.size(), 3 * system.Positions.size());
-            std::vector<Triplet>       coeff;
-            std::vector<float>         diag;
+            Eigen::SparseMatrix<float> M(3 * system.Positions.size(), 3 * system.Positions.size());
+            std::vector<TRP>           A;
+            std::vector<glm::vec3>     b(system.Positions);
             for (std::size_t i = 0; i < system.Positions.size(); i++) {
-                if (system.Fixed[i])
-                    y[i] = system.Positions[i];
-                else
-                    y[i] = system.Positions[i] + ddt * system.Velocities[i] + ddt * ddt * glm::vec3(0, -system.Gravity, 0);
+                y[i] = system.Positions[i] + ddt * system.Velocities[i] + ddt * ddt * glm::vec3(0, -system.Gravity, 0);
+            }
+            partial_g(system, system.Positions, y, b, ddt);
+            partial2_g(system, system.Positions, y, A, ddt);
+            M.setFromTriplets(A.begin(), A.end());
+            VECXF b_v(3 * system.Positions.size());
+            for (int i = 0; i < system.Positions.size(); i++) {
+                for (int j = 0; j < 3; j++) b_v[i * 3 + j] = b[i][j];
+            }
+            LLT   solver(M);
+            VECXF x_v = solver.solve(b_v);
+            for (int i = 0; i < system.Positions.size(); i++) {
+                for (int j = 0; j < 3; j++) {
+                    system.Velocities[i][j] = -x_v[i * 3 + j] / ddt;
+                    system.Positions[i][j]  = system.Positions[i][j] - x_v[i * 3 + j];
+                }
             }
         }
     }
