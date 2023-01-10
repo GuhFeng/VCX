90d29fe17c5eb8b2f0475b947e58b90480d87d72
fgh <fenguhao@stu.pku.edu.cn>
2023-01-10 16:23:32 +0800

    add metirial

--- src/VCX/Labs/Ponit-Cloud/pointcloud.cpp
+++ src/VCX/Labs/Ponit-Cloud/pointcloud.cpp
@@ -16,25 +16,26 @@ void solve(VCX::Engine::SurfaceMesh & mesh, const char * p) {
     open3d::geometry::PointCloud     pc;
     open3d::io::ReadPointCloudOption po;
     open3d::io::ReadPointCloudFromPCD(p, pc, po);
-    pc.EstimateNormals(open3d::geometry::KDTreeSearchParamKNN(), false);
     if (pc.HasNormals()) printf("Has normal!\n");
+    else pc.EstimateNormals(open3d::geometry::KDTreeSearchParamKNN(), false);
     printf("num points: %zu\n", pc.points_.size());
-    auto d = pc.ComputeNearestNeighborDistance();
-    for (int i = 0; i < d.size(); i++) { d[i] *= 1.5; }
-    open3d::geometry::TriangleMesh                  m1;
-    std::shared_ptr<open3d::geometry::TriangleMesh> ms;
-    // ms = m1.CreateFromPointCloudBallPivoting(pc, d);
-    auto out = m1.CreateFromPointCloudPoisson(pc, 8);
-    ms       = std::get<0>(out);
-    open3d::geometry::TriangleMesh m(*ms);
-    for (auto tri : m.triangles_) {
+    auto   distances = pc.ComputeNearestNeighborDistance();
+    double avg_distance =
+        std::accumulate(distances.begin(), distances.end(), 0.0) / distances.size();
+    double                                          rho = 1.5 * avg_distance;
+    std::vector<double>                             radii { 0.005, 0.01, 0.02, 0.04, 0.08, 0.16 };
+    std::shared_ptr<open3d::geometry::TriangleMesh> m(new open3d::geometry::TriangleMesh());
+    // m = m->CreateFromPointCloudBallPivoting(pc, radii);
+    m = m->CreateFromPointCloudAlphaShape(pc, 0.005);
+    for (auto tri : m->triangles_) {
         mesh.Indices.push_back(tri.x());
         mesh.Indices.push_back(tri.y());
         mesh.Indices.push_back(tri.z());
     }
-    for (auto v : m.vertices_) { mesh.Positions.push_back(glm::vec3(1)); }
-    for (int i = 0; i < m.vertices_.size(); i++) {
-        mesh.Positions[i] = glm::vec3(m.vertices_[i].x(), m.vertices_[i].y(), m.vertices_[i].z());
+    for (auto v : m->vertices_) { mesh.Positions.push_back(glm::vec3(1)); }
+    for (int i = 0; i < m->vertices_.size(); i++) {
+        mesh.Positions[i] =
+            glm::vec3(m->vertices_[i].x(), m->vertices_[i].y(), m->vertices_[i].z());
     }
-    int num_points = pc.points_.size();
+    printf("finish!\n");
 }
