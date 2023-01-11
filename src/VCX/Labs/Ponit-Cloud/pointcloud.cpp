dbb83a7ed60bae6c2e0c6d7212b1f60526dd0841
fgh <fenguhao@stu.pku.edu.cn>
2023-01-11 10:38:36 +0800

    qwq

--- src/VCX/Labs/Ponit-Cloud/pointcloud.cpp
+++ src/VCX/Labs/Ponit-Cloud/pointcloud.cpp
@@ -15,19 +15,14 @@
 void solve(VCX::Engine::SurfaceMesh & mesh, const char * p) {
     open3d::geometry::PointCloud     pc;
     open3d::io::ReadPointCloudOption po;
-    open3d::io::ReadPointCloudFromPLY(p, pc, po);
+    open3d::io::ReadPointCloudFromPCD(p, pc, po);
     if (pc.HasNormals()) printf("Has normal!\n");
     else pc.EstimateNormals(open3d::geometry::KDTreeSearchParamKNN(), false);
     printf("num points: %zu\n", pc.points_.size());
-    auto   distances = pc.ComputeNearestNeighborDistance();
-    double avg_distance =
-        std::accumulate(distances.begin(), distances.end(), 0.0) / distances.size();
-    double              rho = 1.25 * avg_distance / 2.0;
-    std::vector<double> radii { 0.005 * rho, 0.1 * rho, 0.2 * rho, 0.4 * rho,
-                                0.8 * rho,   2.0 * rho, 10.0 * rho };
+    std::vector<double>                             radii { 0.06 };
     std::shared_ptr<open3d::geometry::TriangleMesh> m(new open3d::geometry::TriangleMesh());
     m = m->CreateFromPointCloudBallPivoting(pc, radii);
-
+    printf("1\n");
     for (auto tri : m->triangles_) {
         mesh.Indices.push_back(tri.x());
         mesh.Indices.push_back(tri.y());
@@ -38,5 +33,5 @@ void solve(VCX::Engine::SurfaceMesh & mesh, const char * p) {
         mesh.Positions[i] =
             glm::vec3(m->vertices_[i].x(), m->vertices_[i].y(), m->vertices_[i].z());
     }
-    printf("finish!\n");
+    printf("vertice: %ld\ntriangles: %ld\n finish!\n", m->vertices_.size(), m->triangles_.size());
 }
